c77910247c85fad214aa4ba7948dded56214954a
Ergi  Boja <perdoruesperdoruesi@gmail.com>
2022-09-02 02:12:43 +0200

    done

--- index.ts
+++ index.ts
@@ -3,141 +3,101 @@ import express from 'express'
 const cors = require('cors')
 
 const app = express()
+app.use(express.json())
 const port = 4000
 const quotes = [
     {
         id:0,
-    quoteText: "One today is worth two tomorrows.",
-    quoteAuthor: "Benjamin Franklin"
-    },{
-    id:1,
-    quoteText: "Genius is one percent inspiration and ninety-nine percent perspiration.",
-    quoteAuthor: "Thomas Edison"
-    }, {
+     quote: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+     
+    author:{
+        name:"Thomas ",
+        surname:"Edison",
+        photo:'url'
+    } 
+    },
+    {
+        id:1,
+     quote:  "You can observe a lot just by watching.",
+     
+    author:{
+        name: "Yogi ",
+        surname: "Berra",
+        photo:'url'
+    }
+    },
+    {
         id:2,
-    quoteText: "You can observe a lot just by watching.",
-    quoteAuthor: "Yogi Berra"
-    }, {
+     quote: "A house divided against itself cannot stand.",
+     
+    author:{
+        name:"Abraham ",
+        surname:"Lincoln",
+        photo:'url'
+    }
+    },
+    {
         id:3,
-    quoteText: "A house divided against itself cannot stand.",
-    quoteAuthor: "Abraham Lincoln"
-    }, {
+     quote: "Fate is in your hands and no one elses",
+     
+    author:{
+        name:"Byron ",
+        surname:"Pulsifer",
+        photo:'url'
+    } 
+    },
+    {
         id:4,
-    quoteText: "Difficulties increase the nearer we get to the goal.",
-    quoteAuthor: "Johann Wolfgang von Goethe"
-    }, {
+     quote: "Nothing happens unless first we dream.",
+     
+    author:{
+        name:"Carl ",
+        surname:"Sandburg",
+        photo:'url'
+    } 
+    },
+    {
         id:5,
-    quoteText: "Fate is in your hands and no one elses",
-    quoteAuthor: "Byron Pulsifer"
-    }, {
+     quote:  "Be the chief but never the lord.",
+     
+     
+    author:{
+        name:"Lao ",
+        surname:"Tzu",
+        photo:'url'
+    } 
+    },
+    {
         id:6,
-    quoteText: "Be the chief but never the lord.",
-    quoteAuthor: "Lao Tzu"
-    }, {
+     quote: "Well begun is half done.",
+     
+    author:{
+        name:"Aristotle",
+        surname:"",
+        photo:'url'
+    } 
+    },
+    {
         id:7,
-    quoteText: "Nothing happens unless first we dream.",
-    quoteAuthor: "Carl Sandburg"
-    }, {
+     quote: "Life is a learning experience, only if you learn.",
+     
+    author:{
+        name:"Yogi ",
+        surname:"Berra",
+        photo:'url'
+    } 
+    },
+    {
         id:8,
-    quoteText: "Well begun is half done.",
-    quoteAuthor: "Aristotle"
-    }, {
-        id:9,
-    quoteText: "Life is a learning experience, only if you learn.",
-    quoteAuthor: "Yogi Berra"
-    }, {
-        id:10,
-    quoteText: "Self-complacency is fatal to progress.",
-    quoteAuthor: "Margaret Sangster"
-    }, {
-        id:11,
-    quoteText: "Peace comes from within. Do not seek it without.",
-    quoteAuthor: "Buddha"
-    }, {
-        id:12,
-    quoteText: "What you give is what you get.",
-    quoteAuthor: "Byron Pulsifer"
-    }, {
-        id:13,
-    quoteText: "We can only learn to love by loving.",
-    quoteAuthor: "Iris Murdoch"
-    }, {
-        id:14,
-    quoteText: "Life is change. Growth is optional. Choose wisely.",
-    quoteAuthor: "Karen Clark"
-    }, {
-        id:15,
-    quoteText: "You'll see it when you believe it.",
-    quoteAuthor: "Wayne Dyer"
-    }, {
-        id:16,
-    quoteText: "Today is the tomorrow we worried about yesterday.",
-    quoteAuthor: ""
-    }, {
-        id:17,
-    quoteText: "It's easier to see the mistakes on someone else's paper.",
-    quoteAuthor: ""
-    }, {
-        id:18,
-    quoteText: "Every man dies. Not every man really lives.",
-    quoteAuthor: ""
-    }, {
-        id:19,
-    quoteText: "To lead people walk behind them.",
-    quoteAuthor: "Lao Tzu"
-    }, {
-        id:20,
-    quoteText: "Having nothing, nothing can he lose.",
-    quoteAuthor: "William Shakespeare"
-    }, {
-        id:21,
-    quoteText: "Trouble is only opportunity in work clothes.",
-    quoteAuthor: "Henry J. Kaiser"
-    }, {
-        id:22,
-    quoteText: "A rolling stone gathers no moss.",
-    quoteAuthor: "Publilius Syrus"
-    }, {
-        id:23,
-    quoteText: "Ideas are the beginning points of all fortunes.",
-    quoteAuthor: "Napoleon Hill"
-    }, {
-        id:24,
-    quoteText: "Everything in life is luck.",
-    quoteAuthor: "Donald Trump"
-    }, {
-        id:25,
-    quoteText: "Doing nothing is better than being busy doing nothing.",
-    quoteAuthor: "Lao Tzu"
-    }, {
-        id:26,
-    quoteText: "Trust yourself. You know more than you think you do.",
-    quoteAuthor: "Benjamin Spock"
-    }, {
-        id:27,
-    quoteText: "Study the past, if you would divine the future.",
-    quoteAuthor: "Confucius"
-    }, {
-        id:28,
-    quoteText: "The day is already blessed, find peace within it.",
-    quoteAuthor: ""
-    }, {
-        id:29,
-    quoteText: "From error to error one discovers the entire truth.",
-    quoteAuthor: "Sigmund Freud"
-    }, {
-        id:30,
-    quoteText: "Well done is better than well said.",
-    quoteAuthor: "Benjamin Franklin"
-    }, {
-        id:31,
-    quoteText: "Bite off more than you can chew, then chew it.",
-    quoteAuthor: "Ella Williams"
-    }, {
-        id:32,
-    quoteText: "Work out your own salvation. Do not depend on others.",
-    quoteAuthor: "Buddha"
-    }, 
+     quote: "What you give is what you get.",
+     
+    author:{
+        name:"Byron ",
+        surname:"Pulsifer",
+        photo:'url'
+    } 
+    },
+    
 
 ]
 
@@ -149,6 +109,34 @@ app.get('/quotes', cors(),(req, res) => {
 app.get('/random', cors(),(req, res) => {
     res.send(quotes[Math.floor(Math.random() * quotes.length)])
   })
+
+  app.post('/quotes',cors() ,(req, res) => {
+  console.log(req.body)
+  let newitem = {
+    id:quotes[quotes.length - 1].id+1,
+    quote:req.body.quote,
+    author:{
+        name:req.body.author.name,
+        surname:req.body.author.surname,
+        photo:req.body.author.photo
+    }
+  }
+  quotes.push(newitem)
+  res.send(newitem)
+   
+})
+app.get('/quotes/:id', function (req, res) {
+    let id = Number(req.params.id)
+    let match = quotes.find(quote => quote.id === id)
+    if(match){
+        res.send(match);
+    }else{
+        res.status(404).send({ error: 'Quote not found.' })
+        res.send("DataNot Found")
+    }
+    
+    
+    });
 
 app.listen(port, () => {
   console.log(`srv is running in port ${port}`)
